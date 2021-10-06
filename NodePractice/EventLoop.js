4696562ddf3f0937adf5a73743fe9bcaa3e4aa0b
Ahmed Tariq <ahmed.tm097@gmail.com>
2021-10-06 15:07:52 +0500

    update

--- NodePractice/EventLoop.js
+++ NodePractice/EventLoop.js
@@ -1,4 +1,7 @@
 const fs = require('fs');
+const crypto = require('crypto');
+
+const start = Date.now();
 
 setTimeout(() => console.log('Timmer 1 ended'),0);
 setTimeout(() =>console.log('Immediate 1 finished'));
@@ -10,6 +13,12 @@ fs.readFile('eventloop.txt',() =>{
     setTimeout(() => console.log('Timmer 2 ended'),0);
     setTimeout(() => console.log('Timmer 3 ended'),3000);
     setTimeout(() =>console.log('Immediate 2 finished'));
+
+    process.nextTick(() => console.log('Process.nextTick'));
+
+    crypto.pbkdf2('password','salt', 1000, 1024, 'sha512', () =>{
+        console.log(Date.now() - start, 'Password Encrypted');
+    });
 });
 
 console.log('Hello from the top level code');
