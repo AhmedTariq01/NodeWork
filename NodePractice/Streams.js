19001d3df0b21ab9165a0bbb13c22dfef2198b87
Ahmed Tariq <ahmed.tm097@gmail.com>
2021-10-06 19:18:19 +0500

    update

---
+++ NodePractice/Streams.js
@@ -0,0 +1,33 @@
+const fs = require('fs');
+const server = require('http').createServer();
+
+// reading file 
+server.on('request',(req, res) => {
+
+    // solution 1
+    // fs.readFile('./inputdata/data.txt', (err,data) => {
+    //     if (err) console.log('Error reading data');
+    //     res.end(data);
+    // });
+
+    // now by using stream server
+
+    const readable = fs.createReadStream('./inputdata/data.txt');
+    readable.on('data', chunk => {
+        res.write(chunk);
+    });
+    readable.on('end', () => {
+        res.end(); 
+    });
+    readable.on('error', err =>{
+        console.log(err);
+        res.statusCode = 500;
+        res.end('File not found');
+    });
+});
+
+server.listen(8000, '127.0.0.1', () =>{
+    console.log('Server listening');
+});
+
+
