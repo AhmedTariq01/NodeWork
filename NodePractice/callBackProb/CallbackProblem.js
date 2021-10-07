9c81e55e7a418f05fdf5e6c77ce130edcf454c8e
Ahmed Tariq <ahmed.tm097@gmail.com>
2021-10-07 13:18:52 +0500

    end

--- NodePractice/callBackProb/CallbackProblem.js
+++ NodePractice/callBackProb/CallbackProblem.js
@@ -7,6 +7,10 @@ fs.readFile(`dog.txt`, 'utf8', (err, data) => {
     superagent.get(`https://dog.ceo/api/breed/${data}/images/random`).end((err,res) =>{
 
         console.log(res.body.message);
+
+        fs.writeFile(`dogimg.txt`,  res.body.message, err => {
+            console.log('Image saved');
+        });
     });
         
 
