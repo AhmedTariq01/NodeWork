3fc48941ec8a4a41d69a9a90530957cdce59a0da
Ahmed Tariq <ahmed.tm097@gmail.com>
2021-12-01 15:18:50 +0500

    Updates

--- NodePractice/ExpressWork/Restful-PostApi.js
+++ NodePractice/ExpressWork/Restful-PostApi.js
@@ -7,6 +7,7 @@ const app = express();
 // middleware to modify incoming req data
 app.use(morgan('dev'));
 app.use(express.json());
+
 //  show to details of Api response
 app.use((req, res, next) =>{
     console.log('MiddleWare listening');
@@ -20,9 +21,11 @@ app.use((req, res, next) => {
 
 const tours =  JSON.parse(
     fs.readFileSync(`./dev-data/data/tours-simple.json`)
-);
+); 
 
 //  Route Handlers
+
+//  get all tours
 const getAllTours = (req, res) => {
     res.status(200).json({
         status: 'green', 
@@ -33,10 +36,8 @@ const getAllTours = (req, res) => {
     });
 };
 
-app.get('/api/v1/tours', getAllTours);
-
 // only 1 at a time
-app.get('/api/v1/tours/:id'  , (req, res) =>{
+const getTour = (req, res) =>{
     
     console.log(req.params);
     const id = req.params.id * 1;
@@ -62,9 +63,10 @@ app.get('/api/v1/tours/:id'  , (req, res) =>{
             tour
         }
     });
-});
+}
 
-app.post('/api/v1/tours', (req, res) => {
+// Creating a tour
+const createTour = (req, res) => {
     // console.log(req.body);
 
     const newId = tours[tours.length-1].id + 1;
@@ -79,11 +81,10 @@ app.post('/api/v1/tours', (req, res) => {
             }
         });
     });
-});
+};
 
-// patch to update data 
-
-app.patch('/api/v1/tours/:id', (req, res) => {
+// updating the tour
+const updateTour = (req, res) => {
 
     if (req.params.id *1 > tours.length){
         return res.status(404).json({
@@ -98,11 +99,10 @@ app.patch('/api/v1/tours/:id', (req, res) => {
             tour: 'Update tour here'
         }
     })
-});
+};
 
-// delete request
-
-app.delete('/api/v1/tours/:id', (req, res) =>{
+// deleting a tour
+const deleteTour = (req, res) =>{
     if (req.params.id *1 > tours.length){
         return res.status(404).json({
             status: 'red',
@@ -114,7 +114,45 @@ app.delete('/api/v1/tours/:id', (req, res) =>{
         status: 'green',
         data: null
     })
-});
+};
+
+//  Calling All the routes
+// Refactoring the code method 1
+// getting all tours
+app.get('/api/v1/tours', getAllTours);
+//  getting tour by id
+app.get('/api/v1/tours/:id', getTour);
+// creating a tour
+app.post('/api/v1/tours', createTour);
+// patch to update data 
+app.patch('/api/v1/tours/:id', updateTour);
+// delete request
+app.delete('/api/v1/tours/:id', deleteTour);
+// implementing user routes
+// app.get('/api/v1/users', getAllUsers)
+
+// app.route('/api/v1/user/:id').get(getUser).patch(updateUser).delete(deleteUser);
+
+
+//  Calling All the routes
+// Refactoring the code method 2
+
+// getting all tours
+// creating a tour
+app
+   .route('/api/v1/tours')
+   .get(getAllTours)
+   .post(createTour);
+
+//  getting tour by id
+// patch to update data 
+// delete request
+app
+   .route('/api/v1/tour')
+   .get(getTour)
+   .get(updateTour)
+   .get(deleteTour)
+
 
 // Listening
 const port = 3000;
